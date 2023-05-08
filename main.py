d23434549cba4d3316509e68836d41c1fcafb932
Pedro Sag <pedrosag1000@gmail.com>
2023-05-08 02:41:27 -0700

    improve fps by checking image is changed or not

--- main.py
+++ main.py
@@ -28,61 +28,66 @@ class StreamReader:
         self.width=width
         self.height=height
         self.img=frame
+        self.frameId=1
         thread = Thread(target=self.update, args=([]), daemon=True)
         thread.start()
-        print(f"Stream reader initilized with FPS : { fps } width: {width} height: {height}")
+        print(f"Stream reader started with FPS : { fps } width: {width} height: {height}")
 
     # override the run function
     def update(self):
-        capturedFramed=0;
-        lastFPSCalculatedAt=time.time()
+        last_fps_calculated_at=time.time()
         while True:
             ret, self.img = self.stream.read()
-            capturedFramed+=1
+            self.frameId += 1
 
-            if time.time()-lastFPSCalculatedAt >=1:
-                fps=capturedFramed/(time.time()-lastFPSCalculatedAt)
-                capturedFramed=0
-                lastFPSCalculatedAt=time.time()
+            if time.time()-last_fps_calculated_at >= 1:
+                fps=self.frameId/(time.time()-last_fps_calculated_at)
+                self.frameId=0
+                last_fps_calculated_at = time.time()
                 print (f"Real read FPS: {fps}")
 
             
 class StreamViewer:
-    def __init__(self, streamReader:StreamReader,maxWidth=None):
+    def __init__(self, streamReader:StreamReader, max_width=None):
         self.streamReader=streamReader
         
-        if maxWidth==None:
-            maxWidth=streamReader.width
+        if max_width==None:
+            max_width=streamReader.width
 
-        ratio=maxWidth/streamReader.width
+        ratio= max_width / streamReader.width
         self.height=int(ratio*streamReader.height)
-        self.width=int(maxWidth)
+        self.width=int(max_width)
         thread = Thread(target=self.update, args=([]), daemon=True)
         thread.start()
         print(f"Stream viewer initilized with FPS : { streamReader.FPS } width: {self.width} height: {self.height}")
 
     def update(self):
-        capturedFramed=0
-        lastFPSCalculatedAt=time.time()
-        while(True):
+        captured_framed = 0
+        last_frame_id = 0
+        last_fps_calculated_at = time.time()
+        while True:
             
             # Capture the video frame
             # by frame
-            frame = streamReader.img
+
+            while last_frame_id == self.streamReader.frameId:
+                time.sleep(0.5 / stream_reader.FPS)
+
+            frame = self.streamReader.img
+            last_frame_id = self.streamReader.frameId
             
             # Display the resulting frame
-            cv2.imshow('frame', cv2.resize(frame,(self.width,self.height)))
+            cv2.imshow('frame', cv2.resize(frame, (self.width, self.height)))
 
-            capturedFramed+=1
+            captured_framed += 1
 
-            if time.time()-lastFPSCalculatedAt >=1:
-                fps=capturedFramed/(time.time()-lastFPSCalculatedAt)
-                capturedFramed=0
-                lastFPSCalculatedAt=time.time()
+            if time.time()-last_fps_calculated_at >=1:
+                fps = captured_framed/(time.time()-last_fps_calculated_at)
+                captured_framed=0
+                last_fps_calculated_at=time.time()
                 print (f"Show read FPS: {fps}")
-            
-            time.sleep(1/streamReader.FPS)
-            # the 'q' button is set as the````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+            time.sleep(0.5 / stream_reader.FPS)
             # quitting button you may use any
             # desired button of your choice
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -93,16 +98,8 @@ class StreamViewer:
         cv2.destroyAllWindows()
 
 
-            
-
-
-        
-
-
-streamReader=StreamReader("/dev/video1",1920)
-streamViewer=StreamViewer(streamReader,1920)
-
+stream_reader = StreamReader("/dev/video1", 1920)
+stream_viewer = StreamViewer(stream_reader, 1920)
 
 while True:
-
-    time.sleep(1)
+    time.sleep(5)
